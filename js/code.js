e07542a3c02052cd7030b4ca7c6d476dfc966ad8
Brandon971 <r.swinton971@gmail.com>
2022-11-24 16:21:31 +0200

    update

--- js/code.js
+++ js/code.js
@@ -95,7 +95,7 @@ class Calculator {
 
 }
 
-
+//Linking the index.html file with code.js file
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
